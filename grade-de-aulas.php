60267f84e1f2a48c1f1228b89321a86a591fb048
Thais Molica <contato@thaismolica.com.br>
2015-11-07 13:12:35 -0200

    grade de aulas

---
+++ grade-de-aulas.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<?php include 'assets.php'; ?>
+
+		<title>Grade de Aulas - Classe A</title>
+	</head>
+	<body>
+		<div class="container">
+
+			<?php include 'header.php'; ?>
+
+			<main class="main classes">
+
+				<img src="images/grade-de-aulas.jpg">
+				<ul>
+					<li class="taekwon-do">
+						<h2>Taekwon-do</h2>
+						<h3>Grade de Aula</h3>
+					</li>
+					<li class="jiu-jitsu">
+						<h2>Jiu-jitsu</h2>
+						<h3>Grade de Aula</h3>
+					</li>
+					<li class="muay-thai">
+						<h2>Muay-thai</h2>
+						<h3>Grade de Aula</h3>
+					</li>
+					<li class="taefit">
+						<h2>Tae fit</h2>
+						<h3>Grade de Aula</h3>
+					</li>
+					<li class="yoga">
+						<h2>Yoga</h2>
+						<h3>Grade de Aula</h3>
+					</li>
+					<li class="pilates">
+						<h2>Pilates</h2>
+						<h3>Grade de Aula</h3>
+					</li>
+				</ul>
+			</main>
+
+
+			<?php include 'footer.php'; ?>
+		</div>
+
+	</body>
+</html>
