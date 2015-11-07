0443b82284584edc699ffa03eed86a92ba569700
Thais Molica <contato@thaismolica.com.br>
2015-11-07 12:34:23 -0200

    podium

--- header.php
+++ header.php
@@ -3,8 +3,9 @@
   <nav>
     <ul>
       <li><a href="/">Home</a></li>
-      <li><a href="comunidade.php">Comunidade</a></li>
+      <li><a href="classea.php">Classe A</a></li>
       <li><a href="podium.php">Podium</a></li>
+      <li><a href="modalidades.php">Modalidades</a></li>
       <li><a href="grade-de-aulas.php">Grade de aulas</a></li>
       <li><a href="contato.php">Contato</a></li>
     </ul>
---
+++ podium.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<?php include 'assets.php'; ?>
+
+		<title>Podium - Classe A</title>
+	</head>
+	<body>
+		<div class="container">
+
+			<?php include 'header.php'; ?>
+
+			<main class="main">
+
+				<ul class="podium">
+					<li>
+						<h2>Podium Classe A</h2>
+						<div class="img-wrapper"><img src="images/carlos-ficher.jpg" alt="Carlos Ficher"></div>
+						<div class="content">
+							<time>
+								<span>25/10/2015</span>
+								<span>17:25h</span>
+							</time>
+							<p>Nosso aluno Carlos Ficher vence o campeonato mundial de jiu-jitsu</p>
+							<a href="#" class="btn">Leia mais</a>
+						</div>
+					</li>
+					<li>
+						<h2>Noticias</h2>
+						<div class="img-wrapper"><img src="images/eduardo-santoro.jpg" alt="Eduardo Santoro"></div>
+						<div class="content">
+							<time>
+								<span>25/10/2015</span>
+								<span>17:25h</span>
+							</time>
+							<p>Entrevista exclusiva com Eduardo Santoro (Portuga)</p>
+							<a href="#" class="btn">Leia mais</a>
+						</div>
+					</li>
+					<li>
+						<h2>Eventos Classe A</h2>
+						<div class="img-wrapper"><img src="images/semana-gratis.jpg" alt="Semana Gratis"></div>
+						<div class="content">
+							<time>
+								<span>25/10/2015</span>
+								<span>17:25h</span>
+							</time>
+							<p>Semana gratis para todas as categorias de luta</p>
+							<a href="#" class="btn">Leia mais</a>
+						</div>
+					</li>
+					<li>
+						<h2>Podium Classe A</h2>
+						<div class="img-wrapper"><img src="images/richard-kurumada.jpg" alt="Carlos Ficher"></div>
+						<div class="content">
+							<time>
+								<span>25/10/2015</span>
+								<span>17:25h</span>
+							</time>
+							<p>Richard Kurumada se classifica para as finais do mundial de Atlanta</p>
+							<a href="#" class="btn">Leia mais</a>
+						</div>
+					</li>
+				</ul>
+
+			</main>
+
+
+			<?php include 'footer.php'; ?>
+		</div>
+
+	</body>
+</html>
