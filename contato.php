7c1649e71369706fb1c393581161e71c624e8981
Thais Molica <contato@thaismolica.com.br>
2015-11-08 00:10:57 -0200

    contato

---
+++ contato.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<?php include 'assets.php'; ?>
+
+		<title>Contato - Classe A</title>
+	</head>
+	<body>
+		<div class="container">
+
+			<?php include 'header.php'; ?>
+
+			<main class="main">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3655.771234208759!2d-46.670716!3d-23.612536!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce50a750b1d077%3A0x98625eb0eec9eb7a!2sAv.+Ibirapuera%2C+3992+-+Indian%C3%B3polis%2C+S%C3%A3o+Paulo+-+SP%2C+Brasil!5e0!3m2!1spt-BR!2sbr!4v1446946780020" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+				<div>
+					<h2>Informacoes de contato</h2>
+					<p></p>
+				</div>
+			</main>
+
+
+			<?php include 'footer.php'; ?>
+		</div>
+
+	</body>
+</html>
