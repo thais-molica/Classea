75743fced11da4f623e04ad792d410fb18108cb3
erik silva <erik.silva@enterplay.local>
2015-11-11 14:30:17 -0200

    ajustes

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
     <ul>
       <li><a href="/">Home</a></li>
       <li><a href="classea.php">Classe A</a></li>
-      <li><a href="podium.php">Podium</a></li>
       <li><a href="modalidades.php">Modalidades</a></li>
+      <li><a href="podium.php">Podium</a></li>
       <li><a href="grade-de-aulas.php">Grade de aulas</a></li>
       <li><a href="contato.php">Contato</a></li>
     </ul>
