7165afa659a3e27a72c089c87c4400ff2f8665ef
Thais Molica <contato@thaismolica.com.br>
2015-11-20 13:39:56 -0200

    correcao do header

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <header>
-  <a href="/" title="Home" class="logo"><img src="images/logo.png" alt="Classe A"></a>
+  <a href="index.php" title="Home" class="logo"><img src="images/logo.png" alt="Classe A"></a>
   <nav>
     <ul>
-      <li><a href="/index.php">Home</a></li>
+      <li><a href="index.php">Home</a></li>
       <li><a href="classea.php">Classe A</a></li>
-      <li><a href="modalidades.php">Modalidades</a></li>
-      <li><a href="jiu-jitsu.php">Podium</a></li>
+      <li><a href="taekwon-do.php">Modalidades</a></li>
+      <li><a href="podium.php">Podium</a></li>
       <li><a href="grade-de-aulas.php">Grade de aulas</a></li>
       <li><a href="contato.php">Contato</a></li>
     </ul>
